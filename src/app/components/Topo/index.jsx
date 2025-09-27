9a443224f9cf404c4faa584f2df9d1d8068d2316
Manoel Mecias <mmnc12@gmail.com>
2025-09-27 12:21:08 -0300

    refatorando o codigo

---
+++ src/app/components/Topo/index.jsx
@@ -0,0 +1,9 @@
+import estilos from './Topo.module.css';
+
+export default function Topo() {
+  return (
+    <header className={estilos.header}>
+      App de Ufs
+    </header>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,46 +1,13 @@
+import Table from "./components/Table";
+import Topo from "./components/Topo";
 import estilos from "./page.module.css";
-import { retornaUfs } from "./servicos/ufs";
 
 
-export default async function Home() {
-
-  const listaUfs = await retornaUfs();
-
+export default function Home() {
   return (
     <main className={estilos.main}>
-      <header className={estilos.header}>
-        App de Ufs
-      </header>
-      <table className={estilos.table}>
-        <thead>
-          <tr key="">
-            <th>ID</th>
-            <th>UF</th>
-            <th>NOME</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            Array.isArray(listaUfs) ?
-              listaUfs.map((dadosUf) => (
-                <tr key={dadosUf.id}>
-                  <td>{dadosUf.id}</td>
-                  <td>{dadosUf.uf}</td>
-                  <td>{dadosUf.nome}</td>
-                </tr>
-              ))
-              :
-              listaUfs.erro ?
-                <tr>
-                  <td colSpan="3">{listaUfs.erro}</td>
-                </tr>
-                :
-                <tr>
-                  <td colSpan="3">Erro ao consumir API</td>
-                </tr>
-          }
-        </tbody>
-      </table>
+      <Topo />
+      <Table />      
     </main>
   );
 }
