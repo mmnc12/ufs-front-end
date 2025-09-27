bdb2f463fab721305647518269c94de3be875056
Manoel Mecias <mmnc12@gmail.com>
2025-09-27 11:56:08 -0300

    correção no andpoint

--- src/app/servicos/ufs.js
+++ src/app/servicos/ufs.js
@@ -3,8 +3,8 @@ import axios from "axios";
 export async function retornaUfs() {
   try {
     const endpoint = "https://apiufsbrasil.onrender.com/ufs";
-    const {dada} = await axios.get(endpoint, { timeout: 10000 });
-    return dada || [];
+    const {data} = await axios.get(endpoint, { timeout: 10000 });
+    return data || [];
   } catch (error) {
     return { erro: "Erro ao acessar a API DE UFs: " + error.message }
   }
