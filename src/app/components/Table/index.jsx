86177aec34207398732a70f1c95a6923b28ed11b
Manoel Mecias <mmnc12@gmail.com>
2025-09-27 12:35:00 -0300

    refatrando codigo

--- src/app/components/Table/index.jsx
+++ src/app/components/Table/index.jsx
@@ -2,38 +2,38 @@ import { retornaUfs } from '../../service/ufs';
 import estilos from './Table.module.css';
 export default async function Table() {
 
-   const listaUfs = await retornaUfs();
+  const listaUfs = await retornaUfs();
 
   return (
     <table className={estilos.table}>
-        <thead>
-          <tr key="">
-            <th>ID</th>
-            <th>UF</th>
-            <th>NOME</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            Array.isArray(listaUfs) ?
-              listaUfs.map((dadosUf) => (
-                <tr key={dadosUf.id}>
-                  <td>{dadosUf.id}</td>
-                  <td>{dadosUf.uf}</td>
-                  <td>{dadosUf.nome}</td>
-                </tr>
-              ))
-              :
-              listaUfs.erro ?
-                <tr>
-                  <td colSpan="3">{listaUfs.erro}</td>
-                </tr>
-                :
-                <tr>
-                  <td colSpan="3">Erro ao consumir API</td>
-                </tr>
-          }
-        </tbody>
-      </table>
-  )
+      <thead>
+        <tr key="">
+          <th>ID</th>
+          <th>UF</th>
+          <th>NOME</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          Array.isArray(listaUfs) ? (
+            listaUfs.map((dadosUf) => (
+              <tr key={dadosUf.id}>
+                <td>{dadosUf.id}</td>
+                <td>{dadosUf.uf}</td>
+                <td>{dadosUf.nome}</td>
+              </tr>
+            ))
+          ) :
+            listaUfs.erro ? (
+              <tr>
+                <td colSpan="3">{listaUfs.erro}</td>
+              </tr>
+            ) : (
+              <tr>
+                <td colSpan="3">Erro ao consumir API</td>
+              </tr>
+            )}
+      </tbody>
+    </table>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Table from "./components/Table";
 import Topo from "./components/Topo";
 import estilos from "./page.module.css";
 
+export const dynamic = "force-dynamic";
+
 
 export default function Home() {
   return (
