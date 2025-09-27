fd3ea5d1d8de502a775429dddb7c56ccbc9dd231
Manoel Mecias <mmnc12@gmail.com>
2025-09-27 01:05:32 -0300

    ajuste no codigo

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,12 @@ import axios from "axios";
 import estilos from "./page.module.css";
 import { retornaUfs } from "./servicos/ufs";
 
-const listaUfs = await retornaUfs();
+export async function getServerSideProps() {
+  const listaUfs = await retornaUfs();
+  return { props: { listaUfs } };
+}
 
-export default function Home() {
+export default function Home({ listaUfs }) {
   return (
     <main className={estilos.main}>
       <header className={estilos.header}>
@@ -20,23 +23,23 @@ export default function Home() {
         </thead>
         <tbody>
           {
-            Array.isArray(listaUfs) ? 
-            listaUfs.map((dadosUf, index) => (
-              <th key={index}>
-                <td>{dadosUf.id}</td>
-                <td>{dadosUf.uf}</td>
-                <td>{dadosUf.nome}</td>
-              </th>
-            ))
-            :
-            listaUfs.erro ?  
-            <tr>
-              <td colSpan="3">{listaUfs.erro}</td>
-            </tr>
-            :
-            <tr>
-              <td colSpan="3">Erro ao consumir API</td>
-            </tr>
+            Array.isArray(listaUfs) ?
+              listaUfs.map((dadosUf) => (
+                <tr key={dadosUf.id}>
+                  <td>{dadosUf.id}</td>
+                  <td>{dadosUf.uf}</td>
+                  <td>{dadosUf.nome}</td>
+                </tr>
+              ))
+              :
+              listaUfs.erro ?
+                <tr>
+                  <td colSpan="3">{listaUfs.erro}</td>
+                </tr>
+                :
+                <tr>
+                  <td colSpan="3">Erro ao consumir API</td>
+                </tr>
           }
         </tbody>
       </table>
--- src/app/servicos/ufs.js
+++ src/app/servicos/ufs.js
@@ -3,10 +3,8 @@ import axios from "axios";
 export async function retornaUfs() {
   try {
     const endpoint = "https://apiufsbrasil.onrender.com/ufs";
-    const dados = (await axios.get(endpoint, { timeout: 10000 })).data;
-
+    const {dada} = await axios.get(endpoint, { timeout: 10000 });
     return dados;
-
   } catch (error) {
     return { erro: "Erro ao acessar a API DE UFs: " + error.message }
   }
