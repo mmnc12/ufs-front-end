0a29b8fa0254f5e086fdc7d7efaba46bdefcb150
Manoel Mecias <mmnc12@gmail.com>
2025-09-28 10:55:01 -0300

    implementado o campo de busca

--- src/app/components/Topo/index.jsx
+++ src/app/components/Topo/index.jsx
@@ -1,9 +1,27 @@
+'use client'
+import { useState, useEffect } from 'react';
 import estilos from './Topo.module.css';
 
-export default function Topo() {
+export default function Topo({ onBuscar }) {
+  const [termo, setTermo] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onBuscar(termo)
+    }, 500);
+    return () => clearTimeout(timer);
+  }. [termo, onBuscar]);
+
   return (
     <header className={estilos.header}>
       App de Ufs
+      <input 
+        type="text"
+        placeholder="Busca UF ou Nome..."
+        value={termo}
+        onChange={(e) => setTermo(e.target.value)}
+        className={estilos.input}
+      />
     </header>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,26 @@
-import Busca from "./components/Busca";
+import { useState } from "react";
 import Table from "./components/Table";
 import Topo from "./components/Topo";
 import estilos from "./page.module.css";
+import { buscaUfs } from './service/ufs'
 
 export const dynamic = "force-dynamic";
 
 
 export default function Home() {
+  const [listaUfs, setListaUfs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleBuscar = async (termo) => {
+    setLoading(true);
+    const resultados = await buscaUfs(termo);
+    setListaUfs(resultados);
+    setLoading(false);
+  }
   return (
     <main className={estilos.main}>
-      <Topo />
-      <Busca />
-      <Table />      
+      <Topo onBuscar={handleBuscar} />
+      <Table listaUfs={listaUfs} loading={loading} />
     </main>
   );
 }
